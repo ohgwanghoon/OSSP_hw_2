cd280e13b343d47971910ed16002167afaaea557
ohgwanghoon <gwanghun3634@naver.com>
2024-03-24 19:59:40 +0900

    feat : NameId에 팀이름을 입력받고 출력하는 기능 추가

--- NameId.java
+++ NameId.java
@@ -5,18 +5,23 @@ public class NameId {
 		
 		Scanner scanner = new Scanner(System.in);
 		String name;
+		String teamName;
 		int id;
 		
 		System.out.print("이름을 입력하세요: ");
 		name = scanner.next();
 		System.out.print("학번을 입력하세요: ");
 		id = scanner.nextInt();
-		
+		System.out.print("팀이름을 입력하세요: ");
+		teamName = scanner.next();
+		 
 		System.out.println("");
 		System.out.println("<출력>");
 		System.out.println("이름: " + name);
 		System.out.println("학번: " + id);
-		
+		System.out.println("팀이름: " + teamName);
+
+
 		scanner.close();
 	}
 }
